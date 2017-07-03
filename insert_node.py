da8d34e4607f7de64e30fef4ae2b0caa7b8b0e91
Brian Caffey <briancaffey2010@gmail.com>
2017-07-03 18:42:10 -0400

    added insert_node

---
+++ insert_node.py
@@ -0,0 +1,17 @@
+"""
+ Insert Node at the begining of a linked list
+ head input could be None as well for empty list
+ Node is defined as
+
+ class Node(object):
+
+   def __init__(self, data=None, next_node=None):
+       self.data = data
+       self.next = next_node
+
+ return back the head of the linked list in the below method.
+"""
+
+def Insert(head, data):
+    new_head = Node(data=data, next_node=head)
+    return new_head
